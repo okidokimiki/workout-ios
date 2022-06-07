1e6bc4523a66ced329d6abc4b09700f4242e1d08
Mikhail Chaus <setting.chaus@gmail.com>
2022-06-07 18:55:27 +0200

    Unused method removed from AppDelegate.

--- workout/App/AppDelegate.swift
+++ workout/App/AppDelegate.swift
@@ -15,13 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    
-    // MARK: - UIApplicationDelegate
-    
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 }
