d5a2d24be5c09239bc497fecd685303d993e0c76
Jok-ok <nujniy.vorobey@gmail.com>
2022-06-15 17:13:23 +0200

    Creating UIView extension method for cornerRadius.

---
+++ workout/Supporting Files/Extensions/UIView + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView + Extension.swift
+//  workout
+//
+//  Created by Александр Воробей on 15.06.2022.
+//
+
+import UIKit
+
+extension UIView {
+    func roundOffWithRadius(_ cornerRadius: CGFloat) {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
