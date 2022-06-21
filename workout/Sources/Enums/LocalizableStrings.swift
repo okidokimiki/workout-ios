9084726ede587d286f7420f0c179f055fe157e86
Mikhail Chaus <setting.chaus@gmail.com>
2022-06-21 22:55:18 +0200

    Adding separator comments in LocalizableStrings.

--- workout/Sources/Enums/LocalizableStrings.swift
+++ workout/Sources/Enums/LocalizableStrings.swift
@@ -8,18 +8,25 @@
 import Foundation
 
 enum LocalizableStrings: String {
+    // Main
     case tabBarMainSectionTitle = "tabBarMainSection.title"
-    case tabBarStatisticsSectionTitle = "tabBarStatisticsSection.title"
-    case tabBarProfileSectionTitle = "tabBarProfileSection.title"
-    case plusButtonTitle = "plusButton.title"
     case workoutTodayLabelSubtitle = "workoutTodayLabel.subtitle"
-    case noTrainingLabelTitle = "noTrainingLabel.title"
     case noTrainingLabelSubtitle = "noTrainingLabel.subtitle"
-    case weatherLabelTitle = "weatherLabel.title"
-    case newWorkoutLabelTitle = "newWorkoutLabel.title"
+    case noTrainingLabelTitle = "noTrainingLabel.title"
     case weatherLabelSubtitle = "weatherLabel.subtitle"
+    case weatherLabelTitle = "weatherLabel.title"
+    case plusButtonTitle = "plusButton.title"
+    // New Workout
+    case newWorkoutLabelTitle = "newWorkoutLabel.title"
+    // Start Workout
     case startWorkoutLabelTitle = "startWorkoutLabel.title"
+    // Statistics
+    case tabBarStatisticsSectionTitle = "tabBarStatisticsSection.title"
+    // Profile
+    case tabBarProfileSectionTitle = "tabBarProfileSection.title"
+    // Editing profile
     case editingProfileLabelTitle = "editingProfileLabel.title"
+    // Testing
     case blaBlaBlaTitle = "blaBlaBla.title"
         
     var localizedString: String {
